581c1a0e901344c1be5ecc0ea32951f7bbcf06ea
LivingInSyn <jeremymill@gmail.com>
2014-07-03 09:08:25 -0400

    init deletelocaluser
    start of delete local user

---
+++ DeleteLocalUser.ps1
@@ -0,0 +1,64 @@
+param($help, $computer="localhost", $user, $DelProf="n")
+
+ #CREATED BY JEREMY MILL
+ #UITS - UNIVERSITY OF CT
+ #7-3-2014
+ 
+Function Help()
+{
+$helpText=@"
+
+DESCRIPTION: This is a Utility to Delete an account on a local or remote computer
+
+PARAMETERS:
+
+-help	opens this help file
+
+-computer	Specify the computer to run this script. Default is LocalHost
+
+-user	The user to delete
+
+-DelProf	Specify if you want to delete the users profile. Defaults to N
+
+EXAMPLES:
+
+.\DeleteLocalUser -help ?
+
+--prints this help file
+
+.\DeleteLocalUser -computer mycomputer.mydomain.com -user localadmin -delprof Y
+
+--This will delete the local user named LocalAdmin on mycomputer.mydomain.com and delete
+their profile
+
+.\DeleteLocalUser -user localadmin
+
+--This will delete the local user named LocalAdmin on the local machine and will NOT delete
+its profile
+
+"@
+
+$helpText
+	
+exit
+}
+
+#check for help parameter existing
+if($help){ "Obtaining help ..." ; help }
+
+#make sure the user supplied a username to delete.
+if(!$user)
+	{
+	$(Throw "A value for -user is required. Try DeleteLocalUser -help ?")
+	}
+	
+#check if the user exists on the system
+if([ADSI]::Exists('WinNT://./'+$user))
+	{
+	echo "The account exists"
+	$objOu = [ADSI]"WinNT://$computer"
+	$objUser = $objOU.Delete("User", $user)
+	echo "Account Deleted"
+	}
+	
+#if delprof=y, check if it exists and delete it if it does
